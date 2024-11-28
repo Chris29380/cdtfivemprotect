e501fc7dcc8ab4a7663c22ccd0aea570a19b2c03
CDT Dev <148004410+Chris29380@users.noreply.github.com>
2024-11-28 18:32:08 +0100

    debug

--- e403.php
+++ e403.php
@@ -4,11 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>cdtfivemprotect</title>
+    <title>403</title>
 </head>
 <body>
     <div class="content" id="content">
         <img src="/img/logo_fivemprotect.png"/>
+    </div>
+    <div class="footer" id="footer">
+        <?php
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+            echo "<div id='infocli' class='infocli'>Your IP <span style='color:black'>$ip</span></div>";
+        ?>
     </div>
 </body>
 </html>
