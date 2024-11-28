40fbe8b1d99aaf1703949ab8bb49f8a0082cfed7
CDT Dev <148004410+Chris29380@users.noreply.github.com>
2024-11-28 18:28:42 +0100

    Create e403.php

---
+++ e403.php
@@ -0,0 +1,36 @@
+<?php
+    $url = parse_url($_SERVER['REQUEST_URI']);
+
+    $path = isset($url['path']) ? $url['path'] : '/';
+    $parts = explode("&", $url['query']);
+    print_r ($parts[0]);
+
+    if ($parts[0] != '') {
+        $loc = explode("=", $parts[0]);
+        $act = explode("=", $parts[1]);
+        if ($loc[1]="panel") {
+            if ($act[1]="login") {
+                header('Location: /panel/login');
+            } else {
+                if ($act[1]="logout") {
+                    header('Location: /panel/logout');
+                }
+            }
+        }
+    }
+    
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>403</title>
+</head>
+<body>
+    <div class="content" id="content">
+        <img src="/img/logo_fivemprotect.png"/>
+    </div>
+</body>
+</html>
