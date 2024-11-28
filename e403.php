9862b2a26232b82aacc8064b6b4d55e3d0328e9b
CDT Dev <148004410+Chris29380@users.noreply.github.com>
2024-11-28 18:42:20 +0100

    debug

--- e403.php
+++ e403.php
@@ -1,25 +1,3 @@
-<?php
-    $url = parse_url($_SERVER['REQUEST_URI']);
-
-    $path = isset($url['path']) ? $url['path'] : '/';
-    $parts = explode("&", $url['query']);
-    print_r ($parts[0]);
-
-    if ($parts[0] != '') {
-        $loc = explode("=", $parts[0]);
-        $act = explode("=", $parts[1]);
-        if ($loc[1]="panel") {
-            if ($act[1]="login") {
-                header('Location: /panel/login');
-            } else {
-                if ($act[1]="logout") {
-                    header('Location: /panel/logout');
-                }
-            }
-        }
-    }
-    
-?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
